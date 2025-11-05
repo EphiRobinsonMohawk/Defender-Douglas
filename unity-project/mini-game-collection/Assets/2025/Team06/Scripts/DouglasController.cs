1343e23eebf778c705d74baab6f17c850777c52a
Ephi Robinson <000969115@mohawkcollege.ca>
2025-11-05 16:38:01 -0500

    Basic doug

---
+++ unity-project/mini-game-collection/Assets/2025/Team06/Scripts/DouglasController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using MiniGameCollection.Games2025.Team00;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace MiniGameCollection.Games2025.Team06
+{
+    public class DouglasController : MiniGameBehaviour
+    {
+
+        UnityEngine.Vector2 movementInput;
+        [SerializeField] public float dougSpeed;
+        [SerializeField] public PlayerID PlayerID;
+        [SerializeField] public Rigidbody2D rb2d;
+        // Start is called before the first frame update
+
+        // Update is called once per frame
+        void Update()
+        {
+            float axisX = ArcadeInput.Players[(int)PlayerID].AxisX;
+            float axisY = ArcadeInput.Players[(int)PlayerID].AxisY;
+            movementInput = new UnityEngine.Vector2(axisX, axisY);
+            movementInput.Normalize();
+            rb2d.velocity = movementInput * dougSpeed;
+
+
+        }
+        
+
+    
+    }
+
+}
