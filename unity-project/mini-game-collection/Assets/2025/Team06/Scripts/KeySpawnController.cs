9ceb0123fc0e8f7cb010682bf99b8ee935006bd2
Ephi Robinson <000969115@mohawkcollege.ca>
2025-11-18 15:02:21 -0500

    namespace fix

--- unity-project/mini-game-collection/Assets/2025/Team06/Scripts/KeySpawnController.cs
+++ unity-project/mini-game-collection/Assets/2025/Team06/Scripts/KeySpawnController.cs
@@ -1,40 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class KeySpawnController : MonoBehaviour
+namespace MiniGameCollection.Games2025.Team06
 {
-    public float keyTimer;
-    public bool key1Spawned = false;
-    public bool key2Spawned = false;
-    public bool key3Spawned = false;
-    public Transform key1SpawnPoint;
-    public Transform key2SpawnPoint;
-    public Transform key3SpawnPoint;
-    public GameObject keyPrefab;
-
-    // Update is called once per frame
-    void Update()
+    public class KeySpawnController : MonoBehaviour
     {
-        keyTimer += Time.deltaTime;
+        public float keyTimer;
+        public bool key1Spawned = false;
+        public bool key2Spawned = false;
+        public bool key3Spawned = false;
+        public Transform key1SpawnPoint;
+        public Transform key2SpawnPoint;
+        public Transform key3SpawnPoint;
+        public GameObject keyPrefab;
 
-        if (keyTimer >= 20 && !key1Spawned)
+        // Update is called once per frame
+        void Update()
         {
-            Instantiate(keyPrefab, key1SpawnPoint.position, Quaternion.identity);
+            keyTimer += Time.deltaTime;
 
-            key1Spawned = true;
-        }
-        if (keyTimer >= 30 && !key2Spawned)
-        {
-            Instantiate(keyPrefab, key2SpawnPoint.position, Quaternion.identity);
+            if (keyTimer >= 20 && !key1Spawned)
+            {
+                Instantiate(keyPrefab, key1SpawnPoint.position, Quaternion.identity);
 
-            key2Spawned = true;
-        }
-        if (keyTimer >= 40 && !key3Spawned)
-        {
-            Instantiate(keyPrefab, key3SpawnPoint.position, Quaternion.identity);
+                key1Spawned = true;
+            }
+            if (keyTimer >= 30 && !key2Spawned)
+            {
+                Instantiate(keyPrefab, key2SpawnPoint.position, Quaternion.identity);
 
-            key3Spawned = true;
+                key2Spawned = true;
+            }
+            if (keyTimer >= 40 && !key3Spawned)
+            {
+                Instantiate(keyPrefab, key3SpawnPoint.position, Quaternion.identity);
+
+                key3Spawned = true;
+            }
         }
     }
+
 }
