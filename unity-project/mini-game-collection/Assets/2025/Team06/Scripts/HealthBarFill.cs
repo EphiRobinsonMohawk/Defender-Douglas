86918d35063509dc8c926cbd44a20b817b10a252
Ephi Robinson <000969115@mohawkcollege.ca>
2025-11-18 15:05:05 -0500

    namespace update

--- unity-project/mini-game-collection/Assets/2025/Team06/Scripts/HealthBarFill.cs
+++ unity-project/mini-game-collection/Assets/2025/Team06/Scripts/HealthBarFill.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using MiniGameCollection.Games2025.Team06;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class HealthBarFill : MonoBehaviour
+namespace MiniGameCollection.Games2025.Team06
 {
-
-    public Image healthBar;
-    public DouglasController dougScript;
-    public float dougHealth;
-   
-    // Update is called once per frame
-    void Update()
+    public class HealthBarFill : MonoBehaviour
     {
-        dougHealth = (float)dougScript.dougHealth;
-        healthBar.fillAmount = dougHealth * 0.02f;
+
+        public Image healthBar;
+        public DouglasController dougScript;
+        public float dougHealth;
+
+        // Update is called once per frame
+        void Update()
+        {
+            dougHealth = (float)dougScript.dougHealth;
+            healthBar.fillAmount = dougHealth * 0.02f;
+        }
     }
 }
